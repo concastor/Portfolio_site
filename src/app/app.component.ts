a828dc5813f5065daf21671d6e42e1a331c0bb90
Jake Richardson <jake.ejrichardson@gmail.com>
2025-11-12 11:58:37 -0500

    Add site structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './src/app/features/home/home.component';
+import { HeaderComponent } from './src/app/layout/header/header.component';
+import { FooterComponent } from './src/app/layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio-site';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './src/app/features/home/home.component';
+import { ProjectsComponent } from './src/app/features/projects/projects.component';
+import { AboutComponent } from './src/app/features/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Default route
+  { path: 'about', component: AboutComponent },
+  { path: 'portfolio', component: ProjectsComponent },
+];
