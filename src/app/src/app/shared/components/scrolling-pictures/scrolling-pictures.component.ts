2cef551329eed76112d1dc33a953cf35a80f53d1
Jake Richardson <jake.ejrichardson@gmail.com>
2025-11-14 17:50:46 -0500

    add offset on speed

--- src/app/src/app/shared/components/scrolling-pictures/scrolling-pictures.component.ts
+++ src/app/src/app/shared/components/scrolling-pictures/scrolling-pictures.component.ts
@@ -21,53 +21,20 @@ export class ScrollingPicturesComponent implements OnInit {
   @Input({ required: true }) images!: any[];
   @Input() title: string = 'content';
 
-  private animationFrameId: any;
   duplicatedImages: any[] = [];
-  private scrollSpeed = 0.5;
-
-  imageSetWidth = (500 + 25) * this.images?.length || 0;
+  randomOffset = '0ms';
 
   ngOnInit(): void {
     this.duplicatedImages = [...this.images, ...this.images];
+    this.randomOffset = Math.floor(Math.random() * 1000) + 'ms';
 
+    console.log('randome', this.randomOffset);
     // this.autoScroll();
     //set how often it scrolls
     // setInterval(() => {
     //   this.autoScroll();
     // }, 30);
   }
-
-  // autoScroll(): void {
-  //   const animate = () => {
-  //     //throw a hover here later
-  //     const container = this.scrollContainer.nativeElement;
-  //     container.scrollLeft = this.imageSetWidth;
-  //     // const scrollAmount = 1;
-  //     // const maxScroll = container.scrollWidth - container.clientWidth;
-
-  //     container.scrollLeft += this.scrollSpeed;
-  //     console.log('anuimating', container.scrollLeft);
-  //     if (container.scrollLeft >= this.imageSetWidth * 2) {
-  //       container.scrollLeft = this.imageSetWidth;
-  //     }
-
-  //     this.animationFrameId = requestAnimationFrame(animate);
-
-  //     // // If we've reached the end, scroll back to the beginning
-  //     // if (container.scrollLeft >= maxScroll - 1) {
-  //     //   container.scrollTo({
-  //     //     left: 0,
-  //     //     behavior: 'smooth',
-  //     //   });
-  //     // } else {
-  //     //   container.scrollBy({
-  //     //     left: scrollAmount,
-  //     //     behavior: 'smooth',
-  //     //   });
-  //     // }
-  //   };
-  //   this.animationFrameId = requestAnimationFrame(animate);
-  // }
 
   scroll(direction: 'left' | 'right'): void {
     const container = this.scrollContainer.nativeElement;
