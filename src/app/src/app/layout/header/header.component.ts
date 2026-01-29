633f9ecfc21eb77f3d11a3484de8a345009d9752
jake <jake.ejrichardson@gmail.com>
2026-01-29 15:57:12 -0500

    Add header styling

--- src/app/src/app/layout/header/header.component.ts
+++ src/app/src/app/layout/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
-    selector: 'app-header',
-    imports: [RouterLink, RouterLinkActive],
-    templateUrl: './header.component.html',
-    styleUrl: './header.component.scss'
+  selector: 'app-header',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  showDropdown = false;
+}
